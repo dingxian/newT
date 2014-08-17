b90008a569ec71f00bc6ae97a2627aeeaea12671
丁丁贤 <ding>
2014-08-17 15:47:18 +0800

    commit1

---
+++ src/main/java/NumOrder.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2014-08-17.
+ */
+public class NumOrder {
+
+    public Integer[] setOrder(Integer[] nums){
+        int temp = 0;
+        for(int i = 0 ;i<nums.length-1;i++){
+            int numI = nums[i];
+            for(int j = i+1;j<nums.length;j++){
+                int numJ = nums[j];
+                if(numI>numJ){
+                    temp = nums[i];
+                    nums[i] = numJ;
+                    nums[j] = temp;
+                    numI = nums[i];
+                }
+            }
+        }
+        return nums;
+    }
+}
