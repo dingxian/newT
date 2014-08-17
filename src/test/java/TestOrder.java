7ed5682354afdeb7b51ec9688b76a6a2bb8c7e78
丁丁贤 <ding>
2014-08-17 15:47:43 +0800

    commit1

---
+++ src/test/java/TestOrder.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2014-08-17.
+ */
+import org.junit.Test;
+public class TestOrder {
+    /*public static void main(String[] args){
+        Integer[] nums = {2,1,5,7,3,6,4,0};
+        NumOrder numOrder = new NumOrder();
+        Integer[] orderNums = numOrder.setOrder(nums);
+        for(Integer s:orderNums){
+            System.out.println(s);
+        }
+    }*/
+
+    @Test
+    public  void  getOrder(){
+        Integer[] nums = {2,1,5,7,3,6,4,0};
+        NumOrder numOrder = new NumOrder();
+        Integer[] orderNums = numOrder.setOrder(nums);
+        for(Integer s:orderNums){
+            System.out.println(s);
+        }
+    }
+}
